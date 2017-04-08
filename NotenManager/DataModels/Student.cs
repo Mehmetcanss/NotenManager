25463d53bb9d4c14d8ba1630e5e6b00058b0d8c9
Mehmetcan <mehmetcan.sinir@live.com>
2017-04-09 01:22:14 +0200

    dictioanry added

---
+++ NotenManager/DataModels/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotenManager.DataModels
+{
+    public class Student
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _surName;
+
+        public string SurName
+        {
+            get { return _surName; }
+            set { _surName = value; }
+        }
+
+
+
+        public Student(string name, string surname)
+        {
+            this.Name   = name  ;
+            this.SurName = surname;
+        }
+
+        public string DisplayName
+        {
+            get { return this.SurName + ", " + this.Name; }
+            private set {  }
+        }
+
+
+    }
+}
--- NotenManager/MainWindow.xaml.cs
+++ NotenManager/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NotenManager.DataModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,61 @@ namespace NotenManager
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public ObservableCollection<Student> Class1;
+        public ObservableCollection<Student> Class2;
+        public ObservableCollection<Student> Class3;
+        public ObservableCollection<Student> Class4;
+
+        public ObservableCollection<Student> CurrentClass
+        {
+            get { return (ObservableCollection<Student>)GetValue(CurrentClassProperty); }
+            set { SetValue(CurrentClassProperty, value); }
+        }
+
+
+        // Using a DependencyProperty as the backing store for CurrentClass.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CurrentClassProperty =
+            DependencyProperty.Register("CurrentClass", typeof(ObservableCollection<Student>), typeof(MainWindow), new PropertyMetadata(new ObservableCollection<Student>()));
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            InitClasses();
+            this.DataContext = this;
+        }
+
+        private void InitClasses()
+        {
+            Class1 = new ObservableCollection<Student>();
+            Class2 = new ObservableCollection<Student>();
+            Class3 = new ObservableCollection<Student>();
+            Class4 = new ObservableCollection<Student>();
+            Class1.Add(new Student("Mehmetcan", "Sinir"));
+            Class2.Add(new Student("Verena", "Gfüllner"));
+        }
+
+        private void RibbonButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            string tag = (string)b.Tag;            
+            switch(tag)
+            {
+                case "firstClass":
+                    this.CurrentClass = Class1;
+                    break;
+                case "secondClass":
+                    this.CurrentClass = Class2;
+                    break;
+                
+            }
+        }
+
+        private void StudentList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
